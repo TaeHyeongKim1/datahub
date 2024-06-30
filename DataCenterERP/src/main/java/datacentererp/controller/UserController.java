c3baad16b67385387c21cecaef29734dce9838ab
taehe <taehe@LAPTOP-DK23R8VQ>
2024-06-30 20:20:04 +0900

    11111

--- DataCenterERP/src/main/java/datacentererp/controller/UserController.java
+++ DataCenterERP/src/main/java/datacentererp/controller/UserController.java
@@ -1,8 +1,13 @@
 package datacentererp.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import datacentererp.service.UserService;
@@ -16,12 +21,69 @@ public class UserController {
     @PostMapping("/registerCustomer.do")
     public String registerCustomer(@RequestParam String username, @RequestParam String password, @RequestParam String name, @RequestParam String companyNumber) {
         userService.registerCustomer(username, password, name, companyNumber);
-        return "redirect:/login";
+        return "redirect:/login.do";
     }
 
     @PostMapping("/registerAdmin.do")
     public String registerAdmin(@RequestParam String username, @RequestParam String password, @RequestParam String name, @RequestParam String employeeNumber, @RequestParam String position) {
         userService.registerAdmin(username, password, name, employeeNumber, position);
-        return "redirect:/login";
+        return "redirect:/login.do";
     }
+    
+    // 로그인 페이지 이동
+    @GetMapping("/login.do")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    // 회원가입 페이지 이동
+    @GetMapping("/register.do")
+    public String showRegisterPage() {
+        return "register";
+    }
+
+    // 로그인 처리
+    @PostMapping("/login.do")
+    public String login(String username, String password, HttpSession session, Model model) {
+        // 로그인 처리 로직 추가 (데이터베이스 조회 등)
+        if ("admin".equals(username) && "adminpassword".equals(password)) {
+            session.setAttribute("user", username);
+            return "redirect:/main.do";
+        } else if ("customer".equals(username) && "customerpassword".equals(password)) {
+            session.setAttribute("user", username);
+            return "redirect:/main.do";
+        } else {
+            model.addAttribute("error", "Invalid username or password.");
+            return "login";
+        }
+    }
+
+    // 로그아웃 처리
+    @GetMapping("/logout.do")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login.do";
+    }
+
+    // 회원가입 처리 (간단한 예시)
+    @PostMapping("/register.do")
+    public String register(String username, String password, Model model) {
+        // 회원가입 처리 로직 추가 (데이터베이스 저장 등)
+        model.addAttribute("message", "Registration successful. Please login.");
+        return "redirect:/login.do";
+    }
+
+    // 마이페이지 이동
+    @GetMapping("/mypage.do")
+    public String showMyPage(HttpSession session, Model model) {
+        String user = (String) session.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("user", user);
+            return "mypage";
+        } else {
+            return "redirect:/login.do";
+        }
+    }
+
+  
 }
--- DataCenterERP/src/main/webapp/topBar.jsp
+++ DataCenterERP/src/main/webapp/topBar.jsp
@@ -1,35 +1,47 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"
+    import="java.util.*"
+%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:set var="path" value="${pageContext.request.contextPath}" />
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <title>TopBar</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">DataHub <sup>ERP</sup></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <c:choose>
                     <c:when test="${not empty sessionScope.user}">
                         <li class="nav-item">
-                            <a class="nav-link" href="${pageContext.request.contextPath}/logout">Logout</a>
+                            <a class="nav-link" href="${path}/logout.do">Logout</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="${pageContext.request.contextPath}/mypage">My Page</a>
+                            <a class="nav-link" href="${path}/mypage.do">My Page</a>
                         </li>
                     </c:when>
                     <c:otherwise>
                         <li class="nav-item">
-                            <a class="nav-link" href="${pageContext.request.contextPath}/login.jsp">Login</a>
+                            <a class="nav-link" href="${path}/login.do">Login</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="${pageContext.request.contextPath}/register">Sign Up</a>
+                            <a class="nav-link" href="${path}/register.do">Sign Up</a>
                         </li>
                     </c:otherwise>
                 </c:choose>
             </ul>
         </div>
     </nav>
+    <!-- Bootstrap core JavaScript-->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
